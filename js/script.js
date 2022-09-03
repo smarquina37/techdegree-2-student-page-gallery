4f0d85b8b028c559d0ef568ad06c6d8999c9e12b
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-09-02 17:32:13 -0700

    Added studentPage function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,38 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function studentPage(list, page) {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * items 
+   const ul = document.querySelector('.student-list');
+   ul.innerHTML = '';
 
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         
+         const li = document.createElement('li');
+         li.className = 'student-list cf';
+         
+         const div = document.createElement('div');
+         studentDiv.className = 'student-details';
+         
+         const img = document.createElement('img');
+         img.className = 'avatar';
+
+         const h3 = document.createElement('h3');
+
+         const span = document.createElement('span');
+         emailSpan.className = 'email';
+
+         const detailsDiv = document.createElement('div');
+         joinedDiv.className = 'joined-details'
+
+         const dateSpan = document.createElement('span');
+         dateSpan.className = 'date';
+
+      }
+   }
+}
 
 
 /*
